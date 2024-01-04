886470045c53531ca1f292fab72af3cc1cb54fa7
Nagmani kumar <nag2mani@gmail.com>
2024-01-04 21:15:03 +0530

    file made for diffrent purposes

--- a_seed.py
+++ a_seed.py
@@ -1,20 +1,34 @@
+#----------- To make folder in current directory -------------
+
 import os
 from datetime import datetime, timedelta
 
-# folder_name = "POTD"
-# os.makedirs(folder_name, exist_ok=True)
-
-# Set the start date
 start_date = datetime(2024, 1, 2)
-
-# Create 30 files
 for i in range(30):
     current_date = start_date + timedelta(days=i)
     file_name = current_date.strftime("%d_%b_%Y.py")
-    # file_path = os.path.join(folder_name, file_name)
 
-    # with open(file_path, "w") as file:
     with open(file_name, "w") as file:
         file.write("# Your Python code here")
 
 print("Files created successfully in the 'POTD' folder.")
+
+
+
+#----------- To make folder in current directory -------------
+
+import os
+
+folder_path = "C:/Users/nag2m/Desktop/kk"
+
+# Create the folder if it doesn't exist
+os.makedirs(folder_path, exist_ok=True)
+
+for i in range(1, 51):
+    file_name = f"nag{i}.py"
+    file_path = os.path.join(folder_path, file_name)
+
+    with open(file_path, "w") as file:
+        file.write("# Your Python code here")
+
+print(f"Files created successfully in the '{folder_path}' folder.")
