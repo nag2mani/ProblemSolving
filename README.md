dd96cc6a0b344fb084e8d6831585d74c22b46e11
Nagmani kumar <nag2mani@gmail.com>
2024-01-04 21:37:46 +0530

    02 Jan 2024 done and updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# POTD
-This repo will contain all the question with its solution of problem of the day of Geeks for geeks. I am solving one question every day and uploading it on the github.
+This repo will contain questions with its solutions.
+I am going to solve one question every day.
--- a_seed.py
+++ a_seed.py
@@ -11,17 +11,14 @@ for i in range(30):
     with open(file_name, "w") as file:
         file.write("# Your Python code here")
 
-print("Files created successfully in the 'POTD' folder.")
+print("Files created successfully in current folder.")
 
 
 
 #----------- To make folder in current directory -------------
 
 import os
-
-folder_path = "C:/Users/nag2m/Desktop/kk"
-
-# Create the folder if it doesn't exist
+folder_path = "C:/Users/nag2m/Desktop/Problem_Solving/kk"
 os.makedirs(folder_path, exist_ok=True)
 
 for i in range(1, 51):
@@ -29,6 +26,6 @@ for i in range(1, 51):
     file_path = os.path.join(folder_path, file_name)
 
     with open(file_path, "w") as file:
-        file.write("# Your Python code here")
+        file.write(" # Start your code")
 
-print(f"Files created successfully in the '{folder_path}' folder.")
+print("Files created successfully in the folder.")
