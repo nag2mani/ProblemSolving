63a2b149f03c5e502da37ef10cd5fdd8c2078315
Nagmani kumar <nag2mani@gmail.com>
2024-01-05 13:38:10 +0530

    4 jan 2024 solved

--- POTD/04_Jan_2024.py
+++ POTD/04_Jan_2024.py
@@ -1,1 +1,12 @@
-# Your Python code here
+class Solution:
+    def singleElement(self, arr, N):
+        ones = 0
+        twos = 0
+         
+        for i in range(N):
+            twos = twos ^ (ones & arr[i])
+            ones = ones ^ arr[i]
+            common_bit_mask = ~(ones & twos)
+            ones &= common_bit_mask
+            twos &= common_bit_mask
+        return ones
