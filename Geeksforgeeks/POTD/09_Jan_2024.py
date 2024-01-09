391d2f610fb7ac78d514803f23998eeeea30a017
Nagmani kumar <nag2mani@gmail.com>
2024-01-09 22:33:05 +0530

    8th jan done

--- Geeksforgeeks/POTD/09_Jan_2024.py
+++ Geeksforgeeks/POTD/09_Jan_2024.py
@@ -1,1 +1,14 @@
-# Your Python code here
+class Solution:
+    def search(self, pat, txt):
+        indices = []
+        txt_length = len(txt)
+        pat_length = len(pat)
+
+        for i in range(txt_length - pat_length + 1):
+            if txt[i:i+pat_length] == pat:
+                indices.append(i + 1)
+
+        if not indices:
+            return [-1]
+        else:
+            return indices
