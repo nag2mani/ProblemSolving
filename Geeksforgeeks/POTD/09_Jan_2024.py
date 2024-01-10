4f1ba207fc73769ab77bce3b820846eb49b75f9b
Nagmani kumar <nag2mani@gmail.com>
2024-01-10 19:32:45 +0530

    kmp

--- Geeksforgeeks/POTD/09_Jan_2024.py
+++ Geeksforgeeks/POTD/09_Jan_2024.py
@@ -1,16 +1,40 @@
+# Knuth-Morris-Pratt (KMP) String Matching Algorithm
+
 class Solution:
-    def search(self, pat, txt):
-        indices = []
-        txt_length = len(txt)
-        pat_length = len(pat)
+    # def Brutforce(self, pat, txt):
+    # M = len(pat)
+    # N = len(str) 
+    # for i in range(N - M + 1):
+    #     j = 0
+    #     while(j < M):
+    #         if (str[i + j] != pat[j]):
+    #             break
+    #         j += 1
+ 
+    #     if (j == M):
+    #         print("Pattern found at index ", i)
 
-        for i in range(txt_length - pat_length + 1):
-            if txt[i:i+pat_length] == pat:
-                indices.append(i + 1)
 
-        if not indices:
-            return [-1]
-        else:
-            return indices
+
+
+
+
+
+
+
+
+    # def search(self, pat, txt):
+    #     indices = []
+    #     txt_length = len(txt)
+    #     pat_length = len(pat)
+
+    #     for i in range(txt_length - pat_length + 1):
+    #         if txt[i:i+pat_length] == pat:
+    #             indices.append(i + 1)
+
+    #     if not indices:
+    #         return [-1]
+    #     else:
+    #         return indices
 
 
