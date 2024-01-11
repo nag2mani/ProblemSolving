ea8d7ae31acf72be5dc5ebd6766e319669107ab1
Nagmani kumar <nag2mani@gmail.com>
2024-01-11 20:48:41 +0530

    11

--- Geeksforgeeks/POTD/09_Jan_2024.py
+++ Geeksforgeeks/POTD/09_Jan_2024.py
@@ -16,19 +16,19 @@ class Solution:
                 print("Pattern found at index ", i)
 
 
+#method 2 by KMP
+    def search(self, pat, txt):
+        indices = []
+        txt_length = len(txt)
+        pat_length = len(pat)
 
-    # def search(self, pat, txt):
-    #     indices = []
-    #     txt_length = len(txt)
-    #     pat_length = len(pat)
+        for i in range(txt_length - pat_length + 1):
+            if txt[i:i+pat_length] == pat:
+                indices.append(i + 1)
 
-    #     for i in range(txt_length - pat_length + 1):
-    #         if txt[i:i+pat_length] == pat:
-    #             indices.append(i + 1)
-
-    #     if not indices:
-    #         return [-1]
-    #     else:
-    #         return indices
+        if not indices:
+            return [-1]
+        else:
+            return indices
 
 
