52aebbae8c12323182ddffafdd9e744219a5a685
Nagmani kumar <nag2mani@gmail.com>
2024-01-12 18:52:38 +0530

    12th jan done

--- Geeksforgeeks/POTD/12_Jan_2024.py
+++ Geeksforgeeks/POTD/12_Jan_2024.py
@@ -1,5 +1,3 @@
-# method2
-
 from queue import Queue
 
 class Solution:
@@ -32,29 +30,3 @@ class Solution:
         return modified_queue
     
 
-
-    
-
-    def modifyQueue(self, q, k):
-        # Create a temporary queue to store the first k elements
-        temp_queue = Queue()
-        
-        # Dequeue the first k elements from the main queue and enqueue them into the temporary queue
-        for _ in range(k):
-            temp_queue.put(q.get())
-        
-        # Reverse the first k elements using a list
-        reversed_elements = list(temp_queue.queue)[::-1]
-        
-        # Enqueue the reversed elements back into the main queue
-        for element in reversed_elements:
-            q.put(element)
-        
-        # Enqueue the remaining elements back into the main queue
-        for _ in range(q.qsize() - k):
-            q.put(q.get())
-        
-        # Convert the queue to a list for returning
-        modified_queue = list(q.queue)
-        
-        return modified_queue
