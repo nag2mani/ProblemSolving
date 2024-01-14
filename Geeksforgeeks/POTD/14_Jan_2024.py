d35efb9153b6cdaf1b492ae32b0297c2a5cf93af
Nagmani kumar <nag2mani@gmail.com>
2024-01-14 15:25:30 +0530

    14th jan done

--- Geeksforgeeks/POTD/14_Jan_2024.py
+++ Geeksforgeeks/POTD/14_Jan_2024.py
@@ -1,1 +1,12 @@
-# Your Python code here
+class Solution:
+    def repeatedRows(self, arr, m ,n):
+        
+        duplicates_row = []
+        
+        for rows in range(m):
+            for i in range(rows+1, m):
+                if arr[rows] == arr[i]:
+                    if i not in duplicates_row:
+                        duplicates_row.append(i)
+        
+        return sorted(duplicates_row)
