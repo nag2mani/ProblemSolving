650b4d3b87623a50716c194e623da9b09610d47a
Nagmani kumar <nag2mani@gmail.com>
2024-01-16 01:07:12 +0530

    16th jan doing

--- Geeksforgeeks/POTD/16_Jan_2024.py
+++ Geeksforgeeks/POTD/16_Jan_2024.py
@@ -1,1 +1,14 @@
-# Your Python code here
+class Solution:
+    def numberSequence(self, m, n):
+        result = []
+        sub_result = [1]
+        
+        for i in range(n):
+            for j in range(1,m+1):
+                if ((j >= 2 * sub_result[i]) and (j > 0) and (j < m)):
+                    sub_result.append(j)
+                    break
+            result.append(sub_result)
+        
+        return len(result)
+
