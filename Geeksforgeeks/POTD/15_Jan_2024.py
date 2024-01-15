daedf6e3221c78f633531a041fa7342b7eccc4ff
Nagmani kumar <nag2mani@gmail.com>
2024-01-15 11:09:12 +0530

    15th jan done

--- Geeksforgeeks/POTD/15_Jan_2024.py
+++ Geeksforgeeks/POTD/15_Jan_2024.py
@@ -1,1 +1,13 @@
-# Your Python code here
+from typing import List
+
+
+class Solution:
+    def max_courses(self, n : int, total : int, cost : List[int]) -> int:
+        dynamic_array = [0 for _ in range(total + 1)]
+
+        for c in reversed(cost):
+            for t in range(total, c-1, -1):
+                dynamic_array[t] = max(dynamic_array[t], dynamic_array[t - (c + 9)//10] + 1)
+
+        return dynamic_array[total]
+        
