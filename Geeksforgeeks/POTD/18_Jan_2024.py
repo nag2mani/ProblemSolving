0fe0fece2fcd4dd539a8d37f62da6e3921854a45
Nagmani kumar <nag2mani@gmail.com>
2024-01-18 22:56:49 +0530

    18th jan done

--- Geeksforgeeks/POTD/18_Jan_2024.py
+++ Geeksforgeeks/POTD/18_Jan_2024.py
@@ -1,1 +1,35 @@
-# Your Python code here
+#User function Template for python3
+
+class Solution:
+    def min_sprinklers(self, gallery, n):
+        n = len(gallery)
+        
+        # Create a list of ranges
+        ranges = [[0, 0] for _ in range(n)]
+        
+        for i in range(n):
+            if gallery[i] == -1:
+                continue
+            
+            ranges[i][0] = max(0, i - gallery[i])
+            ranges[i][1] = min(n, i + gallery[i])
+        
+        ranges.sort(key=lambda x: x[0])
+        
+        start, i, res = 0, 0, 0
+        curr_max = -1
+        
+        while start < n:
+            while i < n:
+                if ranges[i][0] > start:
+                    break
+                curr_max = max(curr_max, ranges[i][1])
+                i += 1
+            
+            if curr_max < start:
+                return -1
+            
+            res += 1
+            start = curr_max + 1
+        
+        return res
