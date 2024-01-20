84254d4da4dd73d9292ece96e96a953549f2e133
Nagmani kumar <nag2mani@gmail.com>
2024-01-20 19:24:06 +0530

    20th done

--- Geeksforgeeks/POTD/20_Jan_2024.py
+++ Geeksforgeeks/POTD/20_Jan_2024.py
@@ -1,1 +1,29 @@
-# Your Python code here
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+class Solution:
+    def distributeCandy(self, root):
+        moves = [0]  # Initialize the moves count as a list to allow modification within recursive calls
+        self.distribute(root, moves)
+        return moves[0]
+
+    def distribute(self, node, moves):
+        if not node:
+            return 0
+        
+        # Recursively calculate moves for left and right subtrees
+        moves_left = self.distribute(node.left, moves)
+        moves_right = self.distribute(node.right, moves)
+        
+        # Update moves for the current subtree
+        moves[0] += abs(moves_left) + abs(moves_right)
+        
+        # Calculate the net candies at the current node
+        net_candies = node.data + moves_left + moves_right - 1
+        
+        return net_candies
