ef61bf79af1d1985f46d13c1baff4994ba788dc5
nag2mani <nag2mani@gmail.com>
2024-01-31 18:27:07 +0530

    gfg potd done

--- Geeksforgeeks/POTD/31_Jan_2024.py
+++ Geeksforgeeks/POTD/31_Jan_2024.py
@@ -1,1 +1,31 @@
 # Your Python code here
+"""
+class TrieNode: 
+      
+    def __init__(self): 
+        self.children = [None]*26
+  
+        # isEndOfWord is True if node represent the end of the word 
+        self.isEndOfWord = False
+"""
+
+class Solution:
+    def insert(self, root, key):
+        for char in key:
+            nextNode = root.children[ord(char)-97]
+            if not nextNode:
+                newNode = TrieNode()
+                root.children[ord(char)-97] = newNode
+                root = newNode
+            else:
+                root = nextNode
+        root.isEndOfWord = True
+
+    def search(self, root, key):
+        for char in key:
+            nextNode = root.children[ord(char)-97]
+            if not nextNode:
+                return False
+            else:
+                root = nextNode
+        return root.isEndOfWord is True
