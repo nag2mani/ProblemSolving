eb0ef89f91067b34d9338b22538710ee803a3c8f
nag2mani <nag2mani@gmail.com>
2024-01-31 17:59:37 +0530

    potd corrected

--- Leetcode/POTD/31_jan_2024.py
+++ Leetcode/POTD/31_jan_2024.py
@@ -1,4 +1,4 @@
-#Only 35/48 test cases passes.
+#Only 35/48 test cases passes.----------------------------------------------------
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
@@ -20,4 +20,50 @@ class Solution:
     
 
 
-#Other solution
+#Other solution-----------------------------------------------------------------------
+    
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures) - 1, -1, -1):
+            if not stack:
+                stack.append(i)
+                ans[i] = 0
+            else:
+                while stack and temperatures[i] >= temperatures[stack[-1]]:
+                    stack.pop()
+
+                if not stack:
+                    ans[i] = 0
+                else:
+                    ans[i] = stack[-1] - i
+
+                stack.append(i)
+
+        return ans
+
+
+
+
+#Perfect solution.-------------------------------------------------------------------
+
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
+
+    output = [0]*len(temperatures)
+    stack = []
+    for cindex,ctemp  in enumerate(temperatures):
+        while stack and stack[-1][0] < ctemp:#cause it is monotonically decreasing
+                t,i = stack.pop()
+                output[i] = cindex-i
+        stack.append((ctemp, cindex))
+    return output
+with open('user.out', 'w') as f:
+    for case in map(loads, stdin):
+        f.write(f"[{','.join([str(x) for x in dailyTemperatures(case)])}]\n")
+exit()
+
+
+
+
