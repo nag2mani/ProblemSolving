af7b9690a281c47554e22372d2a5729465de76bc
nag2mani <nag2mani@gmail.com>
2024-02-04 19:13:23 +0530

    done up to 4 feb

--- Leetcode/POTD/03_feb_2024.py
+++ Leetcode/POTD/03_feb_2024.py
@@ -1,52 +1,32 @@
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        a, c, d, r = 0, Counter(), Counter(t), [inf, '']
-        for b in range(len(s)):
-            c[s[b]] += 1
-            while all(c[v] >= d[v] for v in d):
-                r = min(r, [b-a+1, s[a:b+1]])
-                c[s[a]] -= 1
-                a += 1
-        
-        return r[1]
-
-
-# more optimised
-
 
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
-            return ""
-
-        char_frequency_t = defaultdict(int)
-        for char in t:
-            char_frequency_t[char] += 1
-
-        left = 0
-        min_len = float('inf')
-        min_left = 0
-        count = len(t)
-
-        for right in range(len(s)):
-            current_char = s[right]
-
-            if current_char in char_frequency_t:
-                char_frequency_t[current_char] -= 1
-                if char_frequency_t[current_char] >= 0:
-                    count -= 1
-
-            while count == 0:
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    min_left = left
-
-                left_char = s[left]
-                if left_char in char_frequency_t:
-                    char_frequency_t[left_char] += 1
-                    if char_frequency_t[left_char] > 0:
-                        count += 1
-                left += 1
-
-        return "" if min_len == float('inf') else s[min_left:min_left + min_len]
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        # Length of the input array
+        n = len(arr)
         
+        # Memoization dictionary
+        memo = {}
+
+        # Recursive function to find the maximum sum after partitioning
+        def dfs(i):
+            # Base case: end of the array
+            if i == n:
+                return 0
+
+            # Check if result for index i is memoized
+            if i in memo:
+                return memo[i]
+
+            curMax = curSum = 0
+
+            # Iterate over the next k elements or until the end of the array
+            for j in range(i, min(i + k, n)):
+                curMax = max(curMax, arr[j])
+                cur = curMax * (j - i + 1) + dfs(j + 1)
+                curSum = max(curSum, cur)
+
+            # Memoize result for index i
+            memo[i] = curSum
+            return curSum
+
+        return dfs(0)
