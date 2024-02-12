ca5ef919dd97b07e712f42f5cc3a624cecb83dd1
nag2mani <nag2mani@gmail.com>
2024-02-12 23:01:58 +0530

    leet done

--- Leetcode/POTD/feb_12_2024.py
+++ Leetcode/POTD/feb_12_2024.py
@@ -5,3 +5,22 @@ class Solution:
         return nums[len(nums)//2]
 
 
+# other solution
+class Solution:
+    def majorityElement(self, nums):
+        counter = 1
+        value = nums[0]
+        
+        for i in range(len(nums)):
+            if i == 0:
+                continue
+            
+            if counter == 0:
+                value = nums[i]
+            
+            if nums[i] == value:
+                counter += 1
+            else:
+                counter -= 1
+        
+        return value
