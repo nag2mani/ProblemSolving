95ff3bbe9799b2b7d3fd71276ddd674777fae092
nag2mani <nag2mani@gmail.com>
2024-02-26 16:07:49 +0530

    gfg potd

---
+++ Geeksforgeeks/POTD/feb_26_2024.py
@@ -0,0 +1,19 @@
+from itertools import combinations, permutations
+
+class Solution:
+	def AllPossibleStrings(self, s):
+		a=[]
+		for i in range(1,len(s)+1):
+			for j in list(combinations(s,i)):
+				a.append("".join(j))
+		# a.sort()
+		return a
+
+
+# sol = Solution()
+# s = 'abc'
+# print(sol.AllPossibleStrings(s))
+
+
+print(list(permutations('ABCD',2)))
+print(list(combinations('ABCD',2)))
