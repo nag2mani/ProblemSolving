b5f7475650441eb6d7613c37fd6b92ef03b80477
nag2mani <nag2mani@gmail.com>
2024-03-06 14:37:38 +0530

    leet potd 4m

--- Leetcode/POTD/03_March_2024.py
+++ Leetcode/POTD/03_March_2024.py
@@ -23,7 +23,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def removeNthFromEnd(self, head, n):
         nd = ListNode(0)
@@ -44,6 +44,29 @@ class Solution:
 
 
 # 04 March 2024
+class Solution:
+    def bagOfTokensScore(self, tokens, power):
+        tokens.sort()
+        n = len(tokens)
+        score = 0
+        max_score = 0
+        left = 0
+        right = n - 1
+        
+        while left <= right:
+            if power >= tokens[left]:
+                power -= tokens[left]
+                score += 1
+                left += 1
+                max_score = max(max_score, score)
+            elif score > 0:
+                power += tokens[right]
+                score -= 1
+                right -= 1
+            else:
+                break
+                
+        return max_score
 
 
 # 05 March 2024
