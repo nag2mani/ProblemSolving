8ffb6776d1f67383b4ea99db35700fa13673c205
nag2mani <nag2mani@gmail.com>
2024-03-12 12:00:06 +0530

    done maximal points

--- Algorithms/Maximal_Points/maximal_algorithm_adsa.py
+++ Algorithms/Maximal_Points/maximal_algorithm_adsa.py
@@ -1,5 +1,6 @@
+import plotly.express as px
 def maximal_points_left_to_right(points):
-    points.sort(key=lambda point: (-point[0], -point[1]))
+    points.sort(key=lambda x: x[0])
     comparisons = 0
     maximal_points = [points[0]]
     for i in range(1, len(points)):
@@ -12,7 +13,7 @@ def maximal_points_left_to_right(points):
 
 
 def maximal_points_right_to_left(points):
-    points.sort(key=lambda point: (-point[0], -point[1]))
+    points.sort(key=lambda x: x[0])
     maximal_points = [points[0]]
     comparisons = 0
     for i in range(1, len(points)):
@@ -25,14 +26,6 @@ def maximal_points_right_to_left(points):
 
 
 def main():
-#     # Read input
-#     n = int(input("Enter the number of points: "))
-#     points = []
-
-#     for i in range(n):
-#         x, y = map(int, input(f"Enter coordinates for point {i+1} (x y): ").split())
-#         points.append((x, y))
-
     # Left to Right variant
     maximal_points_lr, comparisons_lr = maximal_points_left_to_right(points)
     print("\nLeft to Right Variant:")
@@ -47,14 +40,18 @@ def main():
     print("Maximal Points:", maximal_points_rl)
     print("Number of Comparisons:", comparisons_rl)
 
+  # Separate x and y coordinates
+    x_values = [point[0] for point in points]
+    y_values = [point[1] for point in points]
 
-Input_list = [
-    [1, 2], [3, 4], [5, 6], [7, 8], [9, 10],
-    [11, 12], [13, 14], [15, 16], [17, 18], [19, 20],
-    [21, 22], [23, 24], [25, 26], [27, 28], [29, 30],
-    [31, 32], [33, 34], [35, 36], [37, 38], [39, 40],
-    [41, 42], [43, 44]
-]
+  # Create a scatter plot
+    fig = px.scatter(x=x_values, y=y_values, labels={'x': 'X-axis', 'y': 'Y-axis'}, title='Plotting all the Points')
+
+  # Show the plot
+    fig.show()
+
+
+Input_list = [(1, 16), (3, 8), (5, 12), (7, 18), (8, 19), (9, 20), (11, 14), (13, 10), (15, 2), (17, 6), (19, 4), (90, 1)]
 
 points = Input_list
 if __name__ == "__main__":
