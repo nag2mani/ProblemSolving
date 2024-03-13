41882716f7dc3d02a8e79cbebe2979baabc46fdc
nag2mani <nag2mani@gmail.com>
2024-03-13 15:53:33 +0530

    done potd

--- Leetcode/POTD/03_March_2024.py
+++ Leetcode/POTD/03_March_2024.py
@@ -208,4 +208,27 @@ class Solution:
 
 
 
+
+# 13 March 2024
+import math
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        if n==1:
+            return 1
+        nums = [i for i in range(1, n+1)]
+        for i in range(n):
+            if sum(nums[:i]) == sum(nums[i-1:]):
+                return i
+        return -1
+
+
+
+
+
+
+
+
+
+
+
     
