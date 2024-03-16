b8b2592918299fca7a0b83eea1469688fbc3c8c6
nag2mani <nag2mani@gmail.com>
2024-03-16 13:18:52 +0530

    15th march leetcode done

--- Leetcode/POTD/03_March_2024.py
+++ Leetcode/POTD/03_March_2024.py
@@ -268,6 +268,22 @@ class Solution:
         return result
 
 
-
+# 15 March 2024
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        
+        prefix = [1] * n
+        suffix = [1] * n
+        
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        
+        answer = [prefix[i] * suffix[i] for i in range(n)]
+        
+        return answer
 
 
