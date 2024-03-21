4c0d50b24b7fec1e25d1c5553c201b240011ec38
nag2mani <nag2mani@gmail.com>
2024-03-21 18:55:20 +0530

    learned deque

---
+++ Notes/deque1.py
@@ -0,0 +1,30 @@
+from collections import deque
+# Double ended queue.
+
+people = ["Nag", "div", "pankaj", "shek"]
+
+dq = deque(people)
+print(dq)
+dq.rotate(-1)
+print(dq)
+dq.rotate(-2)
+print(dq)
+dq.rotate(0)
+print(dq)
+dq.rotate()
+print(dq)
+dq.rotate(1)
+print(dq)
+dq.rotate(2)
+print(dq)
+
+
+# dq.rotate()
+# dq.remove()
+# dq.appendleft()
+# dq.append()
+# dq.popleft()
+# dq.pop()
+# dq.clear()
+
+print(dq)
