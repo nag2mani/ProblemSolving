507810c8d49da5428ddbfd34dbcfa974a95bb952
nag2mani <nag2mani@gmail.com>
2024-04-14 13:55:43 +0530

    dfs done on undirected on weighted graph

--- Algorithms/Tree/dfs.py
+++ Algorithms/Tree/dfs.py
@@ -1,12 +1,16 @@
 # Adjacency list of the above graph.
 adjacency_list = {'Karlsruhe': ['Augsburg', 'Mannheim'],'Mannheim':['Karlsruhe', 'Frankfurt'],'Augsburg': ['Karlsruhe'], 'Würzburg': ['Nümberg', 'Erfurt', 'Frankfurt'], 'Kassel': ['München', 'Frankfurt'], 'München': ['Kassel'], 'Nümberg':['Stuttgart', 'Würzburg'], 'Erfurt':['Würzburg'],'Stuttgart':['Nümberg'], 'Frankfurt':['Mannheim', 'Würzburg', 'Kassel']}
+adjacency_list = {3:[9, 20], 20:[15, 7, 3], 9:[3], 15:[20], 7:[20]}
+graph=[["node1", "node2", "weight"]]
+
+#for storing visited path.
 visited_list = []
 
 def dfs(node):
     if node not in visited_list:
-        print(node, end=" ")
         visited_list.append(node)
         for i in adjacency_list[node]:
             dfs(i)
 
-dfs('Frankfurt')
+dfs(3)
+print(visited_list)
