c304d30bf4162b3466953926ce891cc2a0b4c1ec
nag2mani <nag2mani@gmail.com>
2024-08-25 07:38:46 +0530

    added 100 days of leetcode

--- Leetcode/problems/959.py
+++ Leetcode/problems/959.py
@@ -14,7 +14,7 @@ def regionsBySlashes(grid):
                 scaled_grid[i * 3][j * 3] = 1
                 scaled_grid[i * 3 + 1][j * 3 + 1] = 1
                 scaled_grid[i * 3 + 2][j * 3 + 2] = 1
-
+    print(scaled_grid)
     # Function to perform DFS on the scaled grid
     def dfs(x, y):
         if x < 0 or x >= n * 3 or y < 0 or y >= n * 3 or scaled_grid[x][y] != 0:
@@ -35,10 +35,69 @@ def regionsBySlashes(grid):
     
     return regions
 
-grid2 = [" /","/ "]
+grid2 = ["\\/","/ "]
 grid3 = [" /\\"," \\/","\\  "]
 grid4 = ["/\\/\\", "\\/\\/", "/\\/\\", "\\/\\\\"]
 
 print(regionsBySlashes(grid2))
 print(regionsBySlashes(grid3))
 print(regionsBySlashes(grid4))
+
+
+
+
+### For better visualiation;
+
+# def regionsBySlashes(grid):
+#     n = len(grid)
+#     # We scale up the grid by a factor of 3x to easily manage regions
+#     scaled_grid = [[0] * (n * 3) for _ in range(n * 3)]
+    
+#     # Mark the scaled grid based on '/' and '\\'
+#     for i in range(n):
+#         for j in range(n):
+#             if grid[i][j] == '/':
+#                 scaled_grid[i * 3][j * 3 + 2] = 1
+#                 scaled_grid[i * 3 + 1][j * 3 + 1] = 1
+#                 scaled_grid[i * 3 + 2][j * 3] = 1
+#             elif grid[i][j] == '\\':
+#                 scaled_grid[i * 3][j * 3] = 1
+#                 scaled_grid[i * 3 + 1][j * 3 + 1] = 1
+#                 scaled_grid[i * 3 + 2][j * 3 + 2] = 1
+    
+#     # Print the scaled grid row by row
+#     for row in scaled_grid:
+#         print(' '.join(map(str, row)))
+
+#     # Function to perform DFS on the scaled grid
+#     def dfs(x, y):
+#         if x < 0 or x >= n * 3 or y < 0 or y >= n * 3 or scaled_grid[x][y] != 0:
+#             return
+#         scaled_grid[x][y] = 1
+#         dfs(x - 1, y)
+#         dfs(x + 1, y)
+#         dfs(x, y - 1)
+#         dfs(x, y + 1)
+    
+#     # Counting the number of regions using DFS
+#     regions = 0
+#     for i in range(n * 3):
+#         for j in range(n * 3):
+#             if scaled_grid[i][j] == 0:
+#                 dfs(i, j)
+#                 regions += 1
+    
+#     return regions
+
+# grid2 = ["\\/", "/ "]
+# grid3 = [" /\\", " \\/", "\\  "]
+# grid4 = ["/\\/\\", "\\/\\/", "/\\/\\", "\\/\\\\"]
+
+# print("Grid 2:")
+# print(regionsBySlashes(grid2))
+
+# print("\nGrid 3:")
+# print(regionsBySlashes(grid3))
+
+# print("\nGrid 4:")
+# print(regionsBySlashes(grid4))
