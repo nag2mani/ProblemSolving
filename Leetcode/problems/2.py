d877e6f524725ea5ee863de41feac892a74c1fbf
Nagmani kumar <nag2mani@gmail.com>
2024-09-24 20:29:34 +0530

    p and c done

--- Leetcode/problems/2.py
+++ Leetcode/problems/2.py
@@ -56,3 +56,4 @@ class Solution:
             l2 = l2.next if l2 else None
 
         return dummy.next
+
--- test.py
+++ test.py
@@ -4,28 +4,3 @@ def infpoint(a):
             return a[i-1]
     return a[i]
 
-def infpoint2(a):
-    start = 0
-    end = len(a)
-    while (start < end - 1):
-        mid = start + (end - start)//2
-        if a[mid] > a[mid+1]:
-            return mid
-        else:
-            
-
-
-a1 = [2,4,50,6,3,1]
-a2 = [2,5,67,80,900, 2]
-a3 = [2,5,60,90]
-a4 = [400, 39, 2,1]
-
-print(infpoint(a1))
-print(infpoint(a2))
-print(infpoint(a3))
-print(infpoint(a4))
-
-print(infpoint2(a1))
-print(infpoint2(a2))
-print(infpoint2(a3))
-print(infpoint2(a4))
